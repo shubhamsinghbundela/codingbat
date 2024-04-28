bc5ed3438e754af71656eca4def54a4e98bd5dfe
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-04-28 20:24:17 +0530

    fixing

--- String-2/DoubleChar.java
+++ String-2/DoubleChar.java
@@ -1,10 +1,9 @@
 
-Given a string, return a string where for every char in the original, there are two chars.
+// Given a string, return a string where for every char in the original, there are two chars.
 
-
-doubleChar("The") → "TThhee"
-doubleChar("AAbb") → "AAAAbbbb"
-doubleChar("Hi-There") → "HHii--TThheerree"
+// doubleChar("The") → "TThhee"
+// doubleChar("AAbb") → "AAAAbbbb"
+// doubleChar("Hi-There") → "HHii--TThheerree"
 
 public class DoubleChar {
     public static void main(String[] args) {
