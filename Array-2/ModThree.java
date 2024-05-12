be0b05bdbfc37d72fcb770a3986f539d03c36076
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-05-12 14:46:56 +0530

    done challenge ModThree

---
+++ Array-2/ModThree.java
@@ -0,0 +1,28 @@
+// Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+
+// modThree([2, 1, 3, 5]) → true
+// modThree([2, 1, 2, 5]) → false
+// modThree([2, 4, 2, 5]) → true
+
+public class ModThree {
+    public static void main(String[] args) {
+        int[] nums = { 2, 1, 3, 5 };
+        System.out.println(modThree(nums));
+    }
+
+    public static boolean modThree(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] % 2 == 0) {
+                if (nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+                    return true;
+                }
+            }
+            if (nums[i] % 2 == 1) {
+                if (nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
