215d090be4203b546277dfe57e2ea1df1547f5ae
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-05-12 12:56:20 +0530

    done challenge isEverywhere

---
+++ Array-2/IsEverywhere.java
@@ -0,0 +1,26 @@
+// isEverywhere
+
+// We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
+
+// isEverywhere([1, 2, 1, 3], 1) → true
+// isEverywhere([1, 2, 1, 3], 2) → false
+// isEverywhere([1, 2, 1, 3, 4], 1) → false
+
+public class IsEverywhere {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 1, 3 };
+        System.out.println(isEverywhere(nums, 2));
+    }
+
+    public static boolean isEverywhere(int[] nums, int val) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == val || nums[i + 1] == val) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
