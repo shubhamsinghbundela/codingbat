72ac353317ae00663387c8b466d64ded38f8444b
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-05-12 12:50:21 +0530

    done challenge Sum28

---
+++ Array-2/Sum28.java
@@ -0,0 +1,31 @@
+
+// Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+
+// sum28([2, 3, 2, 2, 4, 2]) → true
+// sum28([2, 3, 2, 2, 4, 2, 2]) → false
+// sum28([1, 2, 3, 4]) → false
+
+public class Sum28 {
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 2, 2, 4, 2 };
+        int[] nums1 = { 2, 3, 2, 2, 4, 2, 2 };
+        int[] nums2 = { 1, 2, 3, 4 };
+        int[] nums3 = { 2, 3, 2, 2, 4, 2 };
+        int[] nums4 = { 2, 3, 2, 2, 4, 2 };
+        System.out.println(sum28(nums1));
+    }
+
+    public static boolean sum28(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                sum += nums[i];
+            }
+        }
+        if (sum == 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
