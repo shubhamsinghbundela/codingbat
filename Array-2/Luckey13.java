651708e3d129c3b50b13766cc42b7a4d643dad27
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-05-15 21:53:55 +0530

    make changes in problem Luckey13

--- Array-2/Luckey13.java
+++ Array-2/Luckey13.java
@@ -7,16 +7,9 @@
 
 public class Luckey13 {
     public static void main(String[] args) {
-        int[] arr1 = { 0, 2, 4 }; // true
 
-        int[] arr2 = { 1, 2, 3 }; // false
-        int[] arr3 = { 1, 2, 4 }; // false
         int[] arr4 = { 2, 7, 2, 8 }; // true
-        int[] arr5 = { 2, 7, 1, 8 }; // false
-        int[] arr6 = { 3, 7, 2, 8 }; // false
-        int[] arr7 = { 2, 7, 2, 1 }; // false
-        int[] arr8 = { 1, 2 }; // false
-        int[] arr9 = { 2, 2 }; // true
+
         System.out.println(lucky13(arr4));
     }
 
