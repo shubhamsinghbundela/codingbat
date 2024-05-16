d7f041017e1cfe4b3e41c660c7ba4d8c79b7fa26
shubhamsigdar1 <shubhamsigdar@gmail.com>
2024-05-16 17:19:01 +0530

    added challenge ZeroMax

--- Array-2/ZeroMax.java
+++ Array-2/ZeroMax.java
@@ -10,7 +10,7 @@
 
 public class ZeroMax {
     public static void main(String[] args) {
-        int[] nums = { 0, 5, 0, 3 };
+        int[] nums = { 0, 1, 0 };
         int[] arr = zeroMax(nums);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -18,6 +18,19 @@ public class ZeroMax {
     }
 
     public static int[] zeroMax(int[] nums) {
+        int max = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] == 0) {
+                if (max % 2 == 1) {
+                    nums[i] = max;
+                }
+            } else {
+                if (nums[i] % 2 == 1 && max < nums[i]) {
+                    max = nums[i];
+                }
+            }
 
+        }
+        return nums;
     }
 }
